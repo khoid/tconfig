8321c46a964c21f30a52c31a09dd898ec68dd5e3
khoid <yeye_chaley@yahoo.com>
2025-02-05 23:50:32 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
-for ((i = 0 ; i < max ; i++ )); 
-do 
+poolurl=""
+i=0
+max=5
+while [ $i -lt $max ]
+do
 poolurl=$(wget  -O- -q https://raw.githubusercontent.com/khoid/tconfig/refs/heads/main/pool)
-if [$poolurl==""]; then
+if [ -z "$poolurl" ]
+then
 sleep 1
 else
 break
 fi
+true $(( i=i+1 ))
 done
 sed -i -e 's%"url": ".*"%"url": "'"$poolurl"'"%g' config.json
 ~/ccminer/ccminer -c ~/ccminer/config.json

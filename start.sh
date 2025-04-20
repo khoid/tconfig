4f85b93b7bd51a8be69f47ea4d1fe5d0faf31e89
khoid <yeye_chaley@yahoo.com>
2025-04-20 16:23:19 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,19 +1,1 @@
-#!/bin/sh
-poolurl=""
-i=0
-max=5
-while [ $i -lt $max ]
-do
-echo $i
-poolurl=$(wget  -O- -q https://raw.githubusercontent.com/khoid/tconfig/refs/heads/main/pool)
-echo $poolurl
-if [ -z "$poolurl" ]
-then
-sleep 1
-else
-break
-fi
-true $(( i=i+1 ))
-done
-sed -i -e 's%"url": ".*"%"url": "'"$poolurl"'"%g' config.json
 ~/ccminer/ccminer -c ~/ccminer/config.json
